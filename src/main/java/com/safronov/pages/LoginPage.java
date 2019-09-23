3bb39a59991b6b769b4ef0f6f53bcdbc158c59c4
Nikolay <freeway1991@yandex.ru>
2019-09-23 23:58:06 +0300

    Добавлена праметризация Логина и пароля

--- src/main/java/com/safronov/pages/LoginPage.java
+++ src/main/java/com/safronov/pages/LoginPage.java
@@ -1,11 +1,12 @@
 package com.safronov.pages;
 
 import com.safronov.Logger;
+import com.safronov.utils.CommandLine;
 
 public class LoginPage extends AbstractPage {
 
-    private String login = "safrontestat";
-    private String password = "Qazxc12345";
+//    private String login = "safrontestat";
+//    private String password = "Qazxc12345";
 
     private String emailXpath = "//input[@type='email']";
     private String nextBtnXpath = "//*[@id='identifierNext' or @id='passwordNext']";
@@ -13,13 +14,15 @@ public class LoginPage extends AbstractPage {
 
 
     public void setLogin() {
-        Logger.info(String.format("Ввод логина в поле: %s", emailXpath));
+        String login = CommandLine.getInstance().getLogin();
+        String password = CommandLine.getInstance().getPassword();
+        Logger.info(String.format("Ввод логина: %s , в поле:  %s", login, emailXpath));
         driver.setText(emailXpath, login);
-        Logger.info(String.format("Нажатие на клавишу: %s",nextBtnXpath));
+        Logger.info(String.format("Нажатие на клавишу: %s", nextBtnXpath));
         driver.click(nextBtnXpath);
-        Logger.info(String.format("Ввод пароля в поле: %s",passwordXpath));
+        Logger.info(String.format("Ввод пароля: %s , в поле: %s", password, passwordXpath));
         driver.setText(passwordXpath, password);
-        Logger.info(String.format("Нажатие на клавишу: %s",nextBtnXpath));
+        Logger.info(String.format("Нажатие на клавишу: %s", nextBtnXpath));
         driver.click(nextBtnXpath);
     }
 
